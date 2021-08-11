ed7c9ddd420acffcd63b2559a1f122e1a760ba35
hartej-singh-assi <hartejsinghassi@gmail.com>
2021-08-11 23:55:41 +0530

    Instrument & Essentials

--- src/app/component/essential/essential.component.ts
+++ src/app/component/essential/essential.component.ts
@@ -15,14 +15,34 @@ export class EssentialComponent implements OnInit {
   ngOnInit(): void {
     this.essentialproducts=[
       {
-        title: 'one',
-        price: '30',
-        image: ''
+        title: 'Sanitizer Stand',
+        price: '345',
+        image: '../../../assets/images/san stand.jpeg'
       },
       {
-        title: 'two',
-        price: '30',
-        image: ''
+        title: 'Sanitizer Dispenser',
+        price: '292',
+        image: '../../../assets/images/san wall.webp'
+      },
+      {
+        title: 'Small Sanitizer Pump',
+        price: '299',
+        image: '../../../assets/images/small san pump.webp'
+      },
+      {
+        title: 'Sanitizer Manual pump (8L)',
+        price: '1339',
+        image: '../../../assets/images/san pump mach.webp'
+      },
+      {
+        title: 'Surface Disinfectant Spray ',
+        price: '112',
+        image: '../../../assets/images/spray.webp'
+      },
+      {
+        title: 'Portable Oxygen Canister',
+        price: '496',
+        image: '../../../assets/images/oxygen can.webp'
       }
 
     ]
--- src/app/component/instruments/instruments.component.ts
+++ src/app/component/instruments/instruments.component.ts
@@ -15,15 +15,40 @@ export class InstrumentsComponent implements OnInit {
   ngOnInit(): void {
     this.instrumentproducts=[
       {
-        title: 'one',
-        price: '30',
-        image: ''
+        title: 'oximeter',
+        price: '1869',
+        image: '../../../assets/images/oximeter.webp'
       },
       {
-        title: 'two',
-        price: '30',
-        image: ''
-      }
+        title: 'Thermometer',
+        price: '103',
+        image: '../../../assets/images/thermometer.webp'
+      },
+      {
+        title: 'Thermogun',
+        price: '575',
+        image: '../../../assets/images/thermogun.webp'
+      },
+      {
+        title: 'Steamer',
+        price: '120',
+        image: '../../../assets/images/steamer.jpg'
+      },
+      {
+        title: 'steam vapourizer',
+        price: '160',
+        image: '../../../assets/images/e steam vaporizer.webp'
+      },
+      {
+        title: 'Thermoflask bottle',
+        price: '759',
+        image: '../../../assets/images/Thermoflask bottle.webp'
+      },
+      {
+        title: 'Electric Kettle',
+        price: '565',
+        image: '../../../assets/images/kettle.webp'
+      },
 
     ]
   }
